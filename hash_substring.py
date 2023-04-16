565d80b7a38251a992b271dba354f09472e0b3c9
221RDB117 <114361013+AgneseRozenberga@users.noreply.github.com>
2023-04-16 21:33:50 +0000

    Agnese Rozenberga 11. grupa 221RDB117

--- hash_substring.py
+++ hash_substring.py
@@ -1,10 +1,17 @@
 #Agnese Rozenberga 11. grupa 221RDB117
 # python3
-
+import random
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
+    izvelne = input().strip()
+    if izvelne == "F":
+        filename = input().strip()
+        with open(filename) as f:
+            return  (f.readLine().strip(),f.readLine().strip())
+
+    else:
+       
     
     # after input type choice
     # read two lines 
@@ -14,20 +21,50 @@ def read_input():
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+        return (input().rstrip(), input().rstrip())
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
-    print(' '.join(map(str, output)))
+    print(' '.join(str(x) for x in output))
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
 
     # and return an iterable variable
-    return [0]
+    p =10 ** 9 + 7
+    x = random.randint(1,p - 1)
+    lengthp = len(pattern)
+    lengtht = len(text)
+
+    if lengthp>lengtht:
+        return[]
+    
+    ash =0
+    hash = 0
+
+    for i in range (lengthp)
+        ash = (ash * x + ord(pattern[i])) % p
+        hash = (hash* x + ord(text[i]))%part
+
+    powofx = 1
+    for i in range(lengthp - 1):
+        powofx = (powofx * x) % part
+    
+    occurances = []
+    for i in range(lengtht - lengthp+1):
+        if ash == hash:
+            if pattern == text[i:i+lengthp]:
+                occurances.append(i)
+        if i < lengtht - lengthp:
+            hash = ((hash - powofx * ord(text[i]))* x + ord(text[i + lengthp])) % p
+            hash = (hash + p) % p
+
+    return occurances
 
 
 # this part launches the functions
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
+    part, text = read_input()
+    occurances = get_occurrences(pattern, text)
+    print_occurrences(occurances)
 
